83a6b98e1336b57834e143dd26ed0fa6ad3084d1
TienAnh2803 <tienanhv303@gmail.com>
2026-01-08 21:45:18 +0700

    AnimationPlayer

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,14 @@ public class TopDownMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movementInput;
     private Vector2 movement;
+    public float deadZone = 0.1f;
+    private Animator ani;
+    public Camera cam;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,9 +24,27 @@ public class TopDownMovement : MonoBehaviour
         // Lấy input 8 hướng (WASD / Arrow)
         movementInput.x = Input.GetAxisRaw("Horizontal");
         movementInput.y = Input.GetAxisRaw("Vertical");
-
         // Chuẩn hóa vector để đi chéo không nhanh hơn
         movement = movementInput.normalized;
+        float speed = movement.magnitude;
+        ani.SetFloat("Speed", speed);
+
+        CalculateAnimation();
+    }
+
+    void CalculateAnimation()
+    {
+        Vector3 mouseWorld = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mouseWorld - transform.position;
+        direction.Normalize();
+
+        if (direction.magnitude < deadZone)
+        {
+            return;
+        }
+
+        ani.SetFloat("LookX", direction.x);
+        ani.SetFloat("LookY", direction.y);
     }
 
     void FixedUpdate()
